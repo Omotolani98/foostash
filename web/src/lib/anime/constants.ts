9c1a29c90b002ec21f838cb5594a9a147a2a0682
omotolani <olatundeomtolani23@gmail.com>
2026-04-09 20:48:58 +0100

    stick to cli: frontend is not my calling

--- web/src/lib/anime/constants.ts
+++
@@ -1,6 +0,0 @@
-export const DURATION = 300
-export const STAGGER_DELAY = 50
-export const EASING = "easeOutCubic" as const
-export const SLIDE_DISTANCE = 12
-export const TEXT_STAGGER_DELAY = 30
-export const TEXT_DURATION = 250
